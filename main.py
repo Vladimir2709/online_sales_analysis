349145a9645e751c25b6d35d95ff83e168afab36
Vladimir Miljanovic <miljanovic.vladimir27@gmail.com>
2026-08-14 18:14:04 +0200

    Update product details and remove inventory display from main

--- main.py
+++ main.py
@@ -5,17 +5,10 @@ def main():
     manager = ProductManager()
 
 
-    manager.add_product(Product("Laptop", 799.99, 5))
-    manager.add_product(Product("Bezicni mis", 19.99, 50))
-    manager.add_product(Product("Mehanicka tastatura", 55.50, 30))
-    manager.add_product(Product("Monitor 24in", 199.9, 20))
-
-
-    print("=== Lista proizvoda ===")
-    manager.display_products()
-
-    print(f"\nUkupna vrednost imventara: {manager.total_inventory_value():.2f}")
-
+    manager.add_product(Product("Gejmerski Laptop", 799.99, 7))
+    manager.add_product(Product("Bezicni mis", 19.99, 40))
+    manager.add_product(Product("Bezicna tastatura", 55.50, 25))
+    manager.add_product(Product("Monitor 27in", 199.9, 20))
 
 
 if __name__ == "__main__":
